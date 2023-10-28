1d94d7da1fd2a00d2dcc317925d862d0374394ab
Jlouisa <jonathan.louisa@gmail.com>
2023-10-28 02:52:52 +0200

    Add product detail page

--- controllers/hardwareController.js
+++ controllers/hardwareController.js
@@ -1,6 +1,8 @@
 const asyncHandler = require("express-async-handler");
 const { body, validationResult } = require("express-validator");
 const Hardware = require("../models/hardware");
+const Category = require("../models/category");
+const Manufacturer = require("../models/manufacturer");
 
 // Display list of all books.
 exports.catalog = asyncHandler(async function (req, res, next) {
@@ -13,10 +15,14 @@ exports.catalog = asyncHandler(async function (req, res, next) {
 });
 
 exports.product = asyncHandler(async function (req, res, next) {
-  const hardware = await Hardware.findById(req.params.id).exec();
+  const hardware = await Hardware.findById(req.params.id).populate("manufacturer").populate("category").exec();
   res.render("hardware_details", {
     title: hardware.name,
     text: hardware.description,
-    hardware: hardware,
+    category: hardware.category.name,
+    specifications: hardware.specifications,
+    manufacturer: hardware.manufacturer.name,
+    price: Number(hardware.price).toFixed(2),
+    stock: hardware.numberInStock,
   });
 });
